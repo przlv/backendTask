8a5629ef34d101141cbbc01767ad79c02d8d2f37
Vladislav <vladprzlv@yandex.ru>
2024-02-14 10:08:32 +0500

    fix(database): The unique attribute of the batch number has been removed

--- db/models/shift_assignment_model.py
+++ db/models/shift_assignment_model.py
@@ -10,12 +10,10 @@ class ProductDataModel(Base):
 
     id = Column(Integer, primary_key=True)
     unique_product_code = Column(String, unique=True)
-    batch_number = Column(Integer, unique=True)
+    batch_number = Column(Integer)
     batch_date = Column(DateTime)
     is_aggregated = Column(Boolean, default=False)
     aggregated_at = Column(DateTime, nullable=True)
-
-    shift_tasks = relationship("ShiftTaskModel", back_populates="product_data")
 
 
 class ShiftTaskModel(Base):
@@ -28,13 +26,12 @@ class ShiftTaskModel(Base):
     work_center = Column(String)
     shift = Column(String)
     brigade = Column(String)
-    batch_number = Column(Integer, ForeignKey('product_data.batch_number'))
+    batch_number = Column(Integer)
     batch_date = Column(DateTime)
     nomenclature = Column(String)
     ekn_code = Column(String)
     rc_identifier = Column(String)
-    shift_start_datetime = Column(DateTime)
-    shift_end_datetime = Column(DateTime)
+    shift_start_datetime = Column(DateTime(timezone=True))
+    shift_end_datetime = Column(DateTime(timezone=True))
 
-    product_data = relationship("ProductDataModel", back_populates="shift_tasks")
     __table_args__ = (UniqueConstraint('batch_number', 'batch_date', name='unique_batch_number_date'),)
